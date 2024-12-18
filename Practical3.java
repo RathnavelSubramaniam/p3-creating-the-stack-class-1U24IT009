a8f79fcc4480b2826dcdf6083359a39c34196aa7
1U24IT009 <anitha180707@gmail.com>
2024-12-18 06:52:04 +0000

    3

--- Practical3.java
+++ Practical3.java
@@ -1,12 +1,12 @@
 class Stack1 {
     int stck[];
     int top;
-    // Constructor to initialize the stack
+// Constructor to initialize the stack
     Stack1(int s) {
         stck = new int[s];
         top = -1;
     }
-    // Method to push an element into the stack
+// Method to push an element into the stack
     void push(int item) {
         if (top > stck.length - 1) {
             System.out.println("Stack is full");
@@ -14,7 +14,7 @@ class Stack1 {
             stck[++top] = item;
         }
     }
-    // Method to pop an element from the stack
+// Method to pop an element from the stack
     int pop() {
         if (top < 0) {
             System.out.println("Stack is empty!");
@@ -23,7 +23,7 @@ class Stack1 {
             return stck[top--];
         }
     }
-    // Method to peek at the top element of the stack
+// Method to peek at the top element of the stack
     int peek() {
         if (top < 0) {
             System.out.println("Stack is empty!");
@@ -37,7 +37,7 @@ public class Practical3 {
     public static void main(String[] args) {
         Stack1 s1 = new Stack1(4);
 
-        // Push elements into the stack
+// Push elements into the stack
         for (int i = 1; i <= 4; i++) {
             s1.push(i * 5);
         }
@@ -46,7 +46,7 @@ public class Practical3 {
         for (int i = 0; i < 4; i++) {
             System.out.println(s1.pop());
         }       
-        // Peek at the top element of the stack
+// Peek at the top element of the stack
         System.out.println("Top element: " + s1.peek());
     }
 }
